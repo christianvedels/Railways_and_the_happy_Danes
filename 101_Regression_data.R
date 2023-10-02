840a27413f9b844f4958366b880675eef37549e7
christianvedels <christian-vs@sam.sdu.dk>
2023-10-02 22:39:06 +0200

    Update 101_Regression_data.R

--- 101_Regression_data.R
+++ 101_Regression_data.R
@@ -7,3 +7,12 @@
 # ==== Libraries ====
 
 # ==== Load data ====
+railways = read_csv2("Data/Panel_of_railways_in_parishes.csv")
+
+Assembly_houses = read_csv2("Data/Panel_of_assembly_houses.csv")
+Assembly_houses_MA = read_csv2("Data/Panel_of_MA_assembly_houses.csv")
+
+Folk_high_schools = read_csv2("Data/Panel_of_folk_high_schools.csv")
+Folk_high_schools_MA = read_csv2("Data/Panel_of_MA_folk_high_schools.csv")
+
+census = read_csv2("Data/Census_data.csv")  
