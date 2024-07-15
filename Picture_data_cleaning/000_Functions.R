1bf302e758c74f24d52a71ddd63b5ec6fd65ecd4
christianvedels <christian-vs@sam.sdu.dk>
2024-07-15 14:55:55 +0200

    Update 000_Functions.R

--- Picture_data_cleaning/000_Functions.R
+++ Picture_data_cleaning/000_Functions.R
@@ -32,3 +32,63 @@ sub_scandi = function(x){
   return(x)
   
 }
+
+
+# Function to parse JSON safely
+safe_fromJSON = function(x) {
+  require(jsonlite)
+  if (is.na(x) || x == "{}" || x == '[NA]') {
+    return(NA)
+  } else {
+    return(fromJSON(x))
+  }
+}
+
+# Function to clean and parse the data
+clean_aps_data = function(df) {
+  df = df %>%
+    mutate(
+      # Replace single quotes with double quotes in the Emotion Scores column
+      `Emotion Scores` = str_replace_all(`Emotion Scores`, "'", "\""),
+      # Parse Object Location as list of numerics
+      `Object Location` = map(`Object Location`, ~safe_fromJSON(paste0("[", .x, "]"))),
+      # # Parse Face Location as list of numerics, handling "NA" correctly
+      `Face Location` = map(`Face Location`, ~safe_fromJSON(paste0("[", .x, "]"))),
+      # Parse Emotion Scores as a list, handling "{}" correctly
+      `Emotion Scores` = map(`Emotion Scores`, ~safe_fromJSON(.x))
+    ) %>%
+    rename(
+      emotion_score = `Emotion Scores`,
+      object_locaiton = `Object Location`,
+      face_location = `Face Location`
+    ) %>% 
+    # Flatten Emotion Scores into separate columns
+    unnest_wider(emotion_score, names_sep = "_")
+  
+  return(df)
+}
+
+read_aps_data = function(x){
+  df = read_csv(
+    x,
+    col_types = cols(
+      Image = col_character(),
+      Detected = col_character(),
+      `Object Location` = col_character(),
+      Confidence = col_double(),
+      `Face Detected` = col_character(),
+      `Face Location` = col_character(),
+      `Dominant Emotion` = col_character(),
+      `Emotion Scores` = col_character()
+    )
+  )
+  df1 = clean_aps_data(df)
+  
+  return(df1)
+}
+
+
+# Round0
+round0 = function(x, s){
+  return(floor(x/s)*s)
+}
